abf4932057a18704e43732b73e7ae2b8e3cf1249
Atsushi Teramoto <k5013541@kadai.jp>
2017-06-06 22:52:14 +0900

    gpuを別関数で取ってくる。

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -2,25 +2,29 @@
 #define CL_USE_DEPRECATED_OPENCL_2_0_APIS
 #include <CL/cl.hpp>
 
-cl::Device get_GPU()
+std::vector<cl::Device> get_Device_List(const cl_device_type type)
 {
     std::vector<cl::Platform> platforms;
     cl::Platform::get(&platforms);
+    std::vector<cl::Device> device_list;
     for (auto &platform : platforms) {
-        std::cout << static_cast<std::string>(platform.getInfo<CL_PLATFORM_NAME>()) << std::endl;
-
-        std::vector<cl::Device> gpu_devices;
-        platform.getDevices(CL_DEVICE_TYPE_GPU, &gpu_devices);
-        for (auto &device : gpu_devices) {
-            std::cout << "\t" << static_cast<std::string>(device.getInfo<CL_DEVICE_NAME>()) << std::endl;
+        std::vector<cl::Device> devices;
+        platform.getDevices(type, &devices);
+        for (auto &device : devices) {
+            device_list.push_back(device);
         }
     }
+    return device_list;
 }
      
 int main(int argc, char *argv[])
 {
 
-    cl::Device gpu = get_GPU();
+    cl::Device gpu = get_Device_List(CL_DEVICE_TYPE_GPU)[0];
+    cl::Platform platform = gpu.getInfo<CL_DEVICE_PLATFORM>();
+
+    std::cout << static_cast<std::string>(platform.getInfo<CL_PLATFORM_NAME>()) << std::endl;
+    std::cout << static_cast<std::string>(gpu.getInfo<CL_DEVICE_NAME>()) << std::endl;
 
     return 0;
 }
