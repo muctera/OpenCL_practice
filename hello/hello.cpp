a1ac4d4da0191c3b1317a165219d09ae054365ec
Atsushi Teramoto <k5013541@kadai.jp>
2017-06-09 10:38:32 +0900

    fetch_Program

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #define CL_USE_DEPRECATED_OPENCL_2_0_APIS
 #include <CL/cl.hpp>
 
@@ -16,16 +17,36 @@ std::vector<cl::Device> get_Device_List(const cl_device_type type)
     }
     return device_list;
 }
-     
-int main()
-{
 
+cl::Program fetch_Program(int &argc, char ** &argv)
+{
+	argc--;
+	argv++;
+	if (argc == 0) {
+		throw;
+	}
+
+
+	std::ifstream filestream(argv[0]);
+	std::string filestring(
+		(std::istreambuf_iterator<char>(filestream)),
+		std::istreambuf_iterator<char>()
+	);
+
+	std::cout << filestring;
+
+	return cl::Program(filestring);
+}
+     
+int main(int argc, char *argv[])
+{
     cl::Device gpu = get_Device_List(CL_DEVICE_TYPE_GPU)[0];
     cl::Platform platform = gpu.getInfo<CL_DEVICE_PLATFORM>();
 
     std::cout << static_cast<std::string>(platform.getInfo<CL_PLATFORM_NAME>()) << std::endl;
     std::cout << static_cast<std::string>(gpu.getInfo<CL_DEVICE_NAME>()) << std::endl;
 
+	cl::Program program = fetch_Program(argc, argv);
 #ifdef _WIN32
 	system("pause");
 #endif
