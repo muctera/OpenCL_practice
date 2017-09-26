96856d6d5a660dd0a93df660c2c7a3b8191b4ff7
Atsushi Teramoto <k5013541@kadai.jp>
2017-09-26 12:48:09 +0900

    ファイル書き込みを別関数で用意。

--- hll_riemann/main.cpp
+++ hll_riemann/main.cpp
@@ -36,6 +36,27 @@ std::string outputfilename(int &argc, char ** &argv)
 
 	return (boost::format("%s\\outfile.hdf5") % argv[0]).str();
 }
+
+void write_file(
+	  H5::H5File &outputfile
+	, int &group_id
+	, const size_t &shape
+	, const std::vector<std::string> &valname
+	, std::vector<float> &value
+	)
+{
+	const size_t variable_num = valname.size();
+	std::vector<H5::DataSet> dataset(variable_num);
+	H5::DataSpace space(1, &shape);
+	H5::Group group = outputfile.createGroup((boost::format("%d") % group_id++).str());
+	for (size_t i = 0; i < variable_num; i++) {
+		dataset[i] = group.createDataSet(valname[i], H5::PredType::NATIVE_FLOAT, space);
+		dataset[i].write(value.data() + shape * i, H5::PredType::NATIVE_FLOAT);
+		dataset[i].close();
+	}
+	group.close();
+	space.close();
+}
      
 int main(int argc, char *argv[])
 {
@@ -71,45 +92,24 @@ int main(int argc, char *argv[])
 	nextstep.setArg(1, value_buf);
 	nextstep.setArg(2, shape);
 
-	queue.enqueueNDRangeKernel(initialize, cl::NDRange(0), cl::NDRange(shape));
-
-	queue.enqueueReadBuffer(value_buf, CL_TRUE, 0, shape*variable_num * sizeof(float), value.data());
-	for (size_t i = 0; i < shape; i++) {
-		std::cout << i << " " << value.at(i + shape*4) << std::endl;
-	}
 	
 	int group_id = 0;
 	H5::H5File outputfile(outputfilename(argc, argv), H5F_ACC_TRUNC);
-	H5::Group group_init = outputfile.createGroup((boost::format("%d") % group_id++).str());
-	std::vector<H5::DataSet> dataset(variable_num);
-	H5::DataSpace space(1, &shape);
-	for (size_t i = 0; i < variable_num; i++) {
-		dataset[i] = group_init.createDataSet(valname[i], H5::PredType::NATIVE_FLOAT, space);
-		dataset[i].write(value.data() + shape * i, H5::PredType::NATIVE_FLOAT);
-		dataset[i].close();
-	}
-	group_init.close();
 
+	queue.enqueueNDRangeKernel(initialize, cl::NDRange(0), cl::NDRange(shape));
+	queue.enqueueReadBuffer(value_buf, CL_TRUE, 0, shape*variable_num * sizeof(float), value.data());
+	write_file(outputfile, group_id, shape, valname, value);
 
-	for (int i = 0; i < 10; i++) {
+	for (int i = 1; i <= 1024; i++) {
 		queue.enqueueNDRangeKernel(calc_flux, cl::NDRange(0), cl::NDRange(shape - halo));
 		queue.enqueueNDRangeKernel(nextstep, cl::NDRange(halo), cl::NDRange(shape - 2*halo));
+		if (i % 32 == 0) {
+			queue.enqueueReadBuffer(value_buf, CL_TRUE, 0, shape*variable_num * sizeof(float), value.data());
+			write_file(outputfile, group_id, shape, valname, value);
+		}
 	}
 
-	queue.enqueueReadBuffer(value_buf, CL_TRUE, 0, shape*variable_num * sizeof(float), value.data());
-	for (size_t i = 0; i < shape; i++) {
-		std::cout << i << " " << value.at(i + shape*4) << std::endl;
-	}
-
-	H5::Group group = outputfile.createGroup((boost::format("%d") % group_id++).str());
-	for (size_t i = 0; i < variable_num; i++) {
-		dataset[i] = group.createDataSet(valname[i], H5::PredType::NATIVE_FLOAT, space);
-		dataset[i].write(value.data() + shape * i, H5::PredType::NATIVE_FLOAT);
-		dataset[i].close();
-	}
-	group.close();
 	outputfile.close();
-	space.close();
 
 
 #ifdef _WIN32
